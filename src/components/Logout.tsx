48974a6506994052876c6aaf37a4a924a66bc71d
BaeHyeonJik <bhj4060@gmail.com>
2024-11-18 19:14:30 +0900

    REFACTOR: /src/Logout.tsx

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { clearTokens } from '../redux/authSlice';
+import Button from './Button';
 
 const Logout: React.FC = () => {
   const dispatch = useDispatch();
@@ -16,12 +17,14 @@ const Logout: React.FC = () => {
   };
 
   return (
-    <div
-      className="fixed bottom-8 right-8 bg-red-500 text-white p-4 rounded-full shadow-lg cursor-pointer hover:bg-red-600 transition-colors"
+    <Button
+      type="button"
       onClick={handleLogout}
+      to = "/login"
+      children = "로그아웃"
+      className="fixed bottom-8 right-8 bg-red-500 text-white p-4 rounded-full shadow-lg hover:bg-red-600 transition-colors"
     >
-      로그아웃
-    </div>
+    </Button>
   );
 };
 
