46feb38e2d0bba6113dc4b73653cb3f296f76302
BaeHyeonJik <bhj4060@gmail.com>
2024-11-23 01:28:12 +0900

    FEAT: question type

--- src/types/question.ts
+++ src/types/question.ts
@@ -1,5 +1,89 @@
-import Faq from './faq';
 
-export type GetQuestionRequest = { question: string };
 
-export type GetQuestionResponse = { faq: Faq };
+
+export type FrequencyRequest = {
+  startDate: string;
+  endDate: string;
+  period: string;
+  sortOrder: string;
+  limit: string;
+};
+
+export type FrequencyResponse = {
+  status: string;
+  message: string;          
+  data?: {
+    logData?: {
+      startDate?: string;
+      endDate?: string;
+      groupData?: {
+          startDate?: string;
+          endDate?: string;
+          data?: {
+              rank?: number;
+              faq_id?: number;
+              question_ko?: string;
+              count?: number;
+          }[];
+      }[];
+    };
+  };
+};
+
+export type FeedbackRequest = {
+  startDate: string;
+  endDate: string;
+  period: string;
+  sortOrder: string;
+  limit: string;
+};
+
+export type FeedbackResponse = {
+  status: string;
+  message: string;          
+  data?: {
+    logData?: {
+      startDate?: string;
+      endDate?: string;
+      groupData?: {
+          startDate?: string;
+          endDate?: string;
+          data?: {
+              rank?: number;
+              faq_id?: number;
+              question_ko?: string;
+              count?: number;
+          }[];
+      }[];
+    };
+  };
+};
+
+export type LanguageRequest = {
+  startDate: string;
+  endDate: string;
+  period: string;
+  sortOrder: string;
+  limit: string;
+};
+
+export type LanguageResponse = {
+  status: string;
+  message: string;          
+  data?: {
+    logData?: {
+      startDate?: string;
+      endDate?: string;
+      groupData?: {
+        startDate?: string;
+        endDate?: string;
+        data?: {
+          ko_frequency?: number;
+          en_frequency?: number;
+        }
+      }[];
+    }
+  };
+};
+
+
