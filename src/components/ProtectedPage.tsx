12347f09c07743cb16c762ce64961eca6c8289ae
BAE HYEONJIK <bhj4060@gmail.com>
2025-03-15 13:29:57 +0900

    REFACTOR: 코드 개편

--- src/components/ProtectedPage.tsx
+++ src/components/ProtectedPage.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useHobitMutatePostApi } from '../hooks/hobitAdmin';
-import { jwtDecode } from 'jwt-decode';
 import { NewAccessTokenRequest, NewAccessTokenResponse } from '../types/user';
 
 interface ProtectedPageProps {
@@ -11,27 +10,18 @@ interface ProtectedPageProps {
 const ProtectedPage: React.FC<ProtectedPageProps> = ({ children }) => {
   const navigate = useNavigate();
   const NewAccessTokenApi = useHobitMutatePostApi<NewAccessTokenRequest, NewAccessTokenResponse>('auth/refresh');
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
   useEffect(() => {
     const checkTokenExpiration = async () => {
       const response = await NewAccessTokenApi({ credentials: 'include' });
-      if (response.payload?.statusCode === 200) {
-        setIsAuthenticated(true);
-      } else {
+      if (response.payload?.statusCode !== 200) {
         alert("세션이 만료되어 다시 로그인 부탁드립니다.");
-        setIsAuthenticated(false);
         navigate('/login');
       }
     };
 
     checkTokenExpiration();
   }, [NewAccessTokenApi]);
-
-  if (isAuthenticated === false) {
-    // 인증되지 않은 경우 아무것도 렌더링하지 않음
-    return null;
-  }
 
   return <>{children}</>; // 인증된 사용자만 자식 컴포넌트를 렌더링
 };
