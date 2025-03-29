c17d42b9a282b665fb2b64d7ed4a44943c075b7e
BAE HYEONJIK <bhj4060@gmail.com>
2025-03-29 20:32:32 +0900

    REFACTOR: 스피닝 디자인 변경

--- src/components/User/Login/LoginForm.tsx
+++ src/components/User/Login/LoginForm.tsx
@@ -103,10 +103,7 @@ const LoginForm: React.FC<{
             disabled={isLoading}
           >
             {isLoading ? (
-              <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 018 8h-4l3 3 3-3h-4a8 8 0 01-8 8v-4l-3 3 3 3v-4a8 8 0 01-8-8z"></path>
-              </svg>
+              <div className="w-5 h-5 border-t-2 border-white border-solid rounded-full animate-spin"></div>
             ) : '로그인'}
           </button>
         </form>
